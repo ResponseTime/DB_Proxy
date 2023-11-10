7a8815ea7f956ca7caac5ea7610d857b05a9efbc
Looper <aayush.bhattacharjee2002@gmail.com>
2023-11-10 19:54:18 +0530

    Made the code very readable like very readable

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const configurator = require('./schema_config.js');
-const db = require('./db-config.js');
+const configurator = require("./schema_config.js");
+const db = require("./db-config.js");
 const port = 3000;
 
-app.post('/insert/:table', (req, res) => {});
-app.get('/select/:table', (req, res) => {});
-app.get('/description/:table', (req, res) => {});
-app.post('/select/:table', (req, res) => {});
-app.delete('/delete/:table/:id', (req, res) => {});
+app.post("/insert/:table", (req, res) => {});
+app.get("/select/:table/:id", (req, res) => {});
+app.get("/description/:table", (req, res) => {});
+app.post("/select/:table/:id", (req, res) => {});
+app.delete("/delete/:table/:id", (req, res) => {});
 
 app.listen(port, () => {
-	console.log(`App listening at http://localhost:${port}`);
-	configurator.config();
+  console.log(`App listening at http://localhost:${port}`);
+  configurator.config();
 });
--- schema_config.js
+++ schema_config.js
@@ -1,61 +1,56 @@
-const { query } = require('express');
-const fs = require('fs');
-const path = require('path');
-const db = require('./db-config.js');
+const fs = require("fs");
+const path = require("path");
+const db = require("./db-config.js");
 
-function checkTables(tables) {}
-function checkTableColumns(table, columns) {}
+function checkTableColumns(table, columns) {
+  const query = `SELECT column_name FROM information_schema.columns WHERE table_name = $1`;
+  db.query(query, [table], (err, res) => {
+    if (err) {
+      console.error("Error executing query", err);
+      return;
+    }
+
+    const columnNames = res.rows.map((row) => row.column_name);
+    console.log("Column names:", table, columnNames);
+  });
+}
 
 function setCharAt(str, index, chr) {
-	if (index > str.length - 1) return str;
-	return str.substring(0, index) + chr + str.substring(index + 1);
+  if (index > str.length - 1) return str;
+  return str.substring(0, index) + chr + str.substring(index + 1);
 }
 function config() {
-	const schemaFile = JSON.parse(fs.readFileSync(path.join(__dirname, 'schema.json')), 'utf8');
-	for (const table in schemaFile['tables']) {
-		const path = schemaFile['tables'][table];
-		const columns = path.columns;
-		const datatypes = path.datatype;
-		const options = path.options;
-		checkTables(table);
-		checkTableColumns(table, columns);
-		let queryString = '';
-		querieBuilder = 'CREATE TABLE ' + table + ' (';
-		// prettier-ignore
-		for (let i = 0; i < columns.length; i++) {
-      queryString += columns[i] + ' ' + datatypes[i];
-			if (options[i].startsWith('FOREIGN KEY')) {
-        queryString +=" " +options[i].substring(options[i].indexOf('REFERENCES'), options[i].length);
-			} 
-      else {
-        queryString += ' ' + options[i];
-			}
-      querieBuilder+=queryString+",";
-			queryString = '';
-		}
-
-		querieBuilder = setCharAt(querieBuilder, querieBuilder.lastIndexOf(','), ')');
-		db.query(
-			`SELECT EXISTS ( SELECT 1 FROM pg_tables WHERE tablename ='${table}') AS table_existence;`,
-			(err, res) => {
-				if (err) {
-					throw err;
-				}
-				if (!res) {
-					db.query(querieBuilder, (err, result) => {
-						if (err) {
-							throw err;
-						}
-						console.log(result);
-					});
-				}
-			}
-		);
-	}
+  // prettier-ignore
+  const schemaFile = JSON.parse(fs.readFileSync(path.join(__dirname, "schema.json")),"utf8");
+  for (const table in schemaFile["tables"]) {
+    const path = schemaFile["tables"][table];
+    const columns = path.columns;
+    const datatypes = path.datatype;
+    const options = path.options;
+    // prettier-ignore
+    db.query(`SELECT EXISTS ( SELECT 1 FROM pg_tables WHERE tablename ='${table}') AS table_existence;`,(err, res) => {
+        if (err) {
+          throw err;
+        }
+        if (!res) {
+			const columnsDefinition = columns.map((col, i) => `${col} ${datatypes[i]} ${options[i]}`);
+			const queryBuilder = `CREATE TABLE ${table} (${columnsDefinition.join(",")})`;
+          db.query(queryBuilder, (err, result) => {
+            if (err) {
+              throw err;
+            }
+            console.log(result);
+          });
+        } else {
+          checkTableColumns(table, columns);
+        }
+      }
+    );
+  }
 }
 module.exports = {
-	config,
-	demo: () => {
-		console.log('Test');
-	},
+  config,
+  demo: () => {
+    console.log("Test");
+  },
 };
