ffb8d5910cf57e64b626801f06e024473f67d01d
Looper <aayush.bhattacharjee2002@gmail.com>
2023-11-10 15:21:29 +0530

    Schema file ingestion system completed gotta work on handling updates and also optimization

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-require("dotenv").config();
-const express = require("express");
-const { Pool } = require("pg");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const configurator = require("./schema_config.js");
+const configurator = require('./schema_config.js');
+const db = require('./db-config.js');
 const port = 3000;
 
-const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: 5432,
-});
-
-app.post("/insert/:table", (req, res) => {});
-app.get("/select/:table", (req, res) => {});
-app.post("/select/:table", (req, res) => {});
-app.delete("/delete/:table/:id", (req, res) => {});
+app.post('/insert/:table', (req, res) => {});
+app.get('/select/:table', (req, res) => {});
+app.get('/description/:table', (req, res) => {});
+app.post('/select/:table', (req, res) => {});
+app.delete('/delete/:table/:id', (req, res) => {});
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-  configurator.config();
+	console.log(`App listening at http://localhost:${port}`);
+	configurator.config();
 });
--- schema_config.js
+++ schema_config.js
@@ -1,28 +1,61 @@
-const fs = require("fs");
-const path = require("path");
+const { query } = require('express');
+const fs = require('fs');
+const path = require('path');
+const db = require('./db-config.js');
+
+function checkTables(tables) {}
+function checkTableColumns(table, columns) {}
+
+function setCharAt(str, index, chr) {
+	if (index > str.length - 1) return str;
+	return str.substring(0, index) + chr + str.substring(index + 1);
+}
 function config() {
-  const schemaFile = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "schema.json")),
-    "utf8"
-  );
-  for (const key in schemaFile) {
-    for (const value in schemaFile[key]) {
-      const table_name = value;
-      const columns = schemaFile[key][value].columns;
-      const datatypes = schemaFile[key][value]["data-type"];
-      const options = schemaFile[key][value].options;
-      res = "";
-      for (let i = 0; i < columns.length; i++) {
-        res += " " + columns[i] + " : " + datatypes[i] + " : " + options[i];
-      }
-      console.log(table_name);
-      console.log(res);
-    }
-  }
+	const schemaFile = JSON.parse(fs.readFileSync(path.join(__dirname, 'schema.json')), 'utf8');
+	for (const table in schemaFile['tables']) {
+		const path = schemaFile['tables'][table];
+		const columns = path.columns;
+		const datatypes = path.datatype;
+		const options = path.options;
+		checkTables(table);
+		checkTableColumns(table, columns);
+		let queryString = '';
+		querieBuilder = 'CREATE TABLE ' + table + ' (';
+		// prettier-ignore
+		for (let i = 0; i < columns.length; i++) {
+      queryString += columns[i] + ' ' + datatypes[i];
+			if (options[i].startsWith('FOREIGN KEY')) {
+        queryString +=" " +options[i].substring(options[i].indexOf('REFERENCES'), options[i].length);
+			} 
+      else {
+        queryString += ' ' + options[i];
+			}
+      querieBuilder+=queryString+",";
+			queryString = '';
+		}
+
+		querieBuilder = setCharAt(querieBuilder, querieBuilder.lastIndexOf(','), ')');
+		db.query(
+			`SELECT EXISTS ( SELECT 1 FROM pg_tables WHERE tablename ='${table}') AS table_existence;`,
+			(err, res) => {
+				if (err) {
+					throw err;
+				}
+				if (!res) {
+					db.query(querieBuilder, (err, result) => {
+						if (err) {
+							throw err;
+						}
+						console.log(result);
+					});
+				}
+			}
+		);
+	}
 }
 module.exports = {
-  config,
-  demo: () => {
-    console.log(9);
-  },
+	config,
+	demo: () => {
+		console.log('Test');
+	},
 };
