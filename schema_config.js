b07315c3cd31bdf0b66edb175415f8d8d1119260
Looper <aayush.bhattacharjee2002@gmail.com>
2023-11-12 11:45:12 +0530

    changed schema file structure and modified system ingestion system accordingly

--- schema_config.js
+++ schema_config.js
@@ -5,45 +5,59 @@ const db = require("./db-config.js");
 function config() {
   // prettier-ignore
   const schemaFile = JSON.parse(fs.readFileSync(path.join(__dirname, "schema.json")),"utf8");
-  for (const table in schemaFile["tables"]) {
-    const path = schemaFile["tables"][table];
-    const columns = path.columns;
-    const datatypes = path.datatype;
-    const options = path.options;
-    // prettier-ignore
-    db.query(`SELECT EXISTS ( SELECT 1 FROM pg_tables WHERE tablename ='${table}') AS table_existence;`,(err, res) => {
+  Object.keys(schemaFile).forEach((table) => {
+    db.query(
+      `SELECT EXISTS ( SELECT 1 FROM pg_tables WHERE tablename ='${table.toLowerCase()}') AS table_existence;`,
+      (err, res) => {
         if (err) {
           throw err;
         }
-        if (!res) {
-			const columnsDefinition = columns.map((col, i) => `${col} ${datatypes[i]} ${options[i]}`);
-			const queryBuilder = `CREATE TABLE ${table} (${columnsDefinition.join(",")})`;
-          db.query(queryBuilder);
+        const existence = res.rows.map((row) => row.table_existence)[0];
+        if (!existence) {
+          const createTableQuery = `CREATE TABLE ${table.toLowerCase()} (${schemaFile[
+            table
+          ].join(", ")});`;
+          db.query(createTableQuery, (err, res) => {
+            if (err) {
+              throw err;
+            }
+          });
         } else {
-			const query = `SELECT column_name FROM information_schema.columns WHERE table_name = $1`;
-			db.query(query, [table], (err, res) => {
-			  if (err) {
-				console.error("Error executing query", err);
-				return;
-			  }
-			  const columnNames = res.rows.map((row) => row.column_name);
-			  newCols = columns.filter((column) => !columnNames.includes(column));
-			  if(newCols.length > 0) {
-				  let idx = newCols.map((column) => columns.indexOf(column))
-				  let definition = idx.map((i)=>`${columns[i]} ${datatypes[i]} ${options[i]}`)
-				  let queryAdd = `ALTER TABLE ${table} ADD ${definition.join(" ")}`
-				  db.query(queryAdd,(err, res) =>{
-					 if (err) {
-						throw err
-					}
-			  	})
-			}	
-			});
+          const query = `SELECT column_name FROM information_schema.columns WHERE table_name = $1`;
+          db.query(query, [table.toLowerCase()], (err, res) => {
+            if (err) {
+              console.error("Error executing query", err);
+              return;
+            }
+            const columnNames = res.rows.map((row) => row.column_name.trim());
+            columns = schemaFile[table].map((row) => {
+              return row.split(" ")[0].trim().toLowerCase();
+            });
+            let newCols = [];
+            for (let i in columns) {
+              if (!columnNames.includes(columns[i])) {
+                newCols.push(columns[i]);
+              }
+            }
+            let rese = [];
+            for (let i of schemaFile[table]) {
+              if (newCols.includes(i.split(" ")[0])) {
+                rese.push(i);
+              }
+            }
+            if (rese.length > 0) {
+              let addCols = `ALTER TABLE ${table.toLowerCase()} ADD COLUMN ${rese.join(
+                ", ADD COLUMN "
+              )};`;
+              db.query(addCols);
+            }
+          });
         }
       }
     );
-  }
+  });
 }
+
 module.exports = {
   config,
   demo: () => {
