da8975fbe1c1cfe55e5a5bbd851f98d8bc5710e7
Looper <aayush.bhattacharjee2002@gmail.com>
2023-11-12 22:37:49 +0530

    fixed some bugs in schema ingesting system

--- app.js
+++ app.js
@@ -6,9 +6,40 @@ const db = require("./db-config.js");
 const port = 3000;
 
 app.post("/insert/:table", (req, res) => {});
-app.get("/select/:table/:id", (req, res) => {});
-app.get("/description/:table", (req, res) => {});
-app.post("/select/:table/:id", (req, res) => {});
+
+app.get("/select/:table/:id", (req, res) => {
+  const table_name = req.query.table;
+  const id = req.query.id;
+  const query = `SELECT * FROM ${table_name} WHERE id = ${id};`;
+  db.query(query, (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    }
+    res.status(200).json({ data: result.rows[0] });
+  });
+});
+
+app.get("/description/:table", (req, res) => {
+  const table_name = req.query.table;
+  const query = `SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_name = ${table_name};`;
+  db.query(query, (err, result) => {
+    if (result.rows.length === 0) {
+      res.status(404).json({ err: `Table '${table_name}' not found.` });
+    } else {
+      const colArr = [];
+      result.rows.forEach((row) => {
+        colArr.push({
+          table_name: row.table_name,
+          column_name: row.column_name,
+          data_type: row.data_type,
+        });
+      });
+      res.status(200).json({ data: colArr });
+    }
+  });
+});
+
+app.post("/update/:table/:id", (req, res) => {});
 app.delete("/delete/:table/:id", (req, res) => {});
 
 app.listen(port, () => {
--- schema_config.js
+++ schema_config.js
@@ -1,60 +1,65 @@
 const fs = require("fs");
 const path = require("path");
-const db = require("./db-config.js");
-
-function config() {
+const d = require("./db-config.js");
+async function config() {
+  const db = await d.connect();
   // prettier-ignore
   const schemaFile = JSON.parse(fs.readFileSync(path.join(__dirname, "schema.json")),"utf8");
-  Object.keys(schemaFile).forEach((table) => {
-    db.query(
-      `SELECT EXISTS ( SELECT 1 FROM pg_tables WHERE tablename ='${table.toLowerCase()}') AS table_existence;`,
-      (err, res) => {
-        if (err) {
-          throw err;
-        }
-        const existence = res.rows.map((row) => row.table_existence)[0];
-        if (!existence) {
-          const createTableQuery = `CREATE TABLE ${table.toLowerCase()} (${schemaFile[
-            table
-          ].join(", ")});`;
-          db.query(createTableQuery, (err, res) => {
-            if (err) {
-              throw err;
-            }
-          });
-        } else {
-          const query = `SELECT column_name FROM information_schema.columns WHERE table_name = $1`;
-          db.query(query, [table.toLowerCase()], (err, res) => {
-            if (err) {
-              console.error("Error executing query", err);
-              return;
-            }
-            const columnNames = res.rows.map((row) => row.column_name.trim());
-            columns = schemaFile[table].map((row) => {
-              return row.split(" ")[0].trim().toLowerCase();
-            });
-            let newCols = [];
+  // prettier-ignore
+  const checkExistence = async (table) => {
+    const resultSet = await db.query(`SELECT EXISTS ( SELECT 1 FROM pg_tables WHERE tablename ='${table.toLowerCase()}') AS table_existence;`);
+    const existence = resultSet.rows.map((row) => row.table_existence)[0];
+    return existence;
+  };
+  // prettier-ignore
+  const createTable = async (table) => {
+    const createTableQuery = `CREATE TABLE ${table.toLowerCase()} (${schemaFile[table].join(", ")});`;
+    return await db.query(createTableQuery);
+  };
+  // prettier-ignore
+  const checkColumns = async (table) => {
+    const query = `SELECT column_name FROM information_schema.columns WHERE table_name = $1`;
+    const resultSet = await db.query(query, [table.toLowerCase()]);
+    return resultSet;
+  }
+  // prettier-ignore
+  const alterTable = async (table,rese) => {
+    let addCols = `ALTER TABLE ${table.toLowerCase()} ADD COLUMN ${rese.join(", ADD COLUMN ")};`;
+    return await db.query(addCols);
+  }
+  // prettier-ignore
+  Object.keys(schemaFile).forEach(async(table) => {
+    try {  
+      const exist = await checkExistence(table);
+      console.log(exist," ",table);
+      if (!exist) {
+        const res = await createTable(table);
+        console.log(res)
+      } else {
+        const resultSet = await checkColumns(table);
+        const columnNames = resultSet.rows.map((row) => row.column_name.trim());
+        let columns = schemaFile[table].map((row) => { return row.split(" ")[0].trim().toLowerCase()});
+        let newCols = [];
             for (let i in columns) {
               if (!columnNames.includes(columns[i])) {
                 newCols.push(columns[i]);
               }
             }
-            let rese = [];
-            for (let i of schemaFile[table]) {
-              if (newCols.includes(i.split(" ")[0])) {
-                rese.push(i);
-              }
-            }
-            if (rese.length > 0) {
-              let addCols = `ALTER TABLE ${table.toLowerCase()} ADD COLUMN ${rese.join(
-                ", ADD COLUMN "
-              )};`;
-              db.query(addCols);
-            }
-          });
+        console.log(newCols)
+        let rese = [];
+        for (let i of schemaFile[table]) {
+          if (newCols.includes(i.split(" ")[0].toLowerCase())) {
+              rese.push(i);
+          }
+        }
+        if (rese.length > 0) {
+          const res = await alterTable(table,rese);
+          console.log(res)
         }
       }
-    );
+    } catch (err) {
+      console.error(err);
+    }
   });
 }
 
