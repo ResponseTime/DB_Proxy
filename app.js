772acb4371c84fcf689de9af26d96c7c56a635c5
Looper <aayush.bhattacharjee2002@gmail.com>
2023-11-13 17:36:15 +0530

    completed all the routes and manually tested them

--- app.js
+++ app.js
@@ -2,47 +2,107 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const configurator = require("./schema_config.js");
-const db = require("./db-config.js");
+const d = require("./db-config.js");
 const port = 3000;
+let db;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post("/insert/:table", (req, res) => {});
-
-app.get("/select/:table/:id", (req, res) => {
-  const table_name = req.query.table;
-  const id = req.query.id;
-  const query = `SELECT * FROM ${table_name} WHERE id = ${id};`;
-  db.query(query, (err, result) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
+app.post("/insert/:table", async (req, res) => {
+  try {
+    const table_name = req.params.table;
+    const insertBody = req.body;
+    const columns = [];
+    const values = [];
+    Object.keys(insertBody).forEach((element) => {
+      columns.push(element.toLowerCase());
+      values.push(insertBody[element]);
+    });
+    // prettier-ignore
+    let query = `INSERT INTO ${table_name} (${columns.join(", ")}) VALUES (${values.map((val,i)=>{return '$'+(i+1)})});`;
+    console.log(query);
+    const res = await db.query(query, values);
+    if (res) {
+      res.status(500).json(res);
     }
-    res.status(200).json({ data: result.rows[0] });
-  });
+    res.status(200).json({ insert: true });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
-app.get("/description/:table", (req, res) => {
-  const table_name = req.query.table;
-  const query = `SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_name = ${table_name};`;
-  db.query(query, (err, result) => {
-    if (result.rows.length === 0) {
-      res.status(404).json({ err: `Table '${table_name}' not found.` });
+app.get("/select/:table/:id", async (req, res) => {
+  try {
+    const table_name = req.params.table;
+    const id = req.params.id;
+    const query = `SELECT * FROM ${table_name} WHERE id = ${id}`;
+    const result = await db.query(query);
+    if (result.rowCount < 1) {
+      res.status(404).json({ err: "Data for Id not found" });
     } else {
-      const colArr = [];
-      result.rows.forEach((row) => {
-        colArr.push({
-          table_name: row.table_name,
-          column_name: row.column_name,
-          data_type: row.data_type,
-        });
-      });
-      res.status(200).json({ data: colArr });
+      res.status(200).json(result.rows[0]);
     }
-  });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+app.get("/desc/:table", async (req, res) => {
+  try {
+    const table_name = req.params.table;
+    const query = `SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_name = '${table_name}';`;
+    const result = await db.query(query);
+    const colArr = [{ table: table_name }];
+    result.rows.forEach((row) => {
+      colArr.push({
+        column_name: row.column_name,
+        data_type: row.data_type,
+      });
+    });
+    res.json(colArr);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+app.post("/update/:table/:id", async (req, res) => {
+  try {
+    const table_name = req.params.table;
+    const id = req.params.id;
+    const update = req.body.update;
+    const updateCols = [];
+    const values = [];
+    Object.keys(update).forEach((element) => {
+      updateCols.push(element);
+      values.push(update[element]);
+    });
+    const query = `UPDATE ${table_name} SET ${updateCols.map((val, i) => {
+      return val + "=" + "($" + (i + 1 + ")");
+    })} WHERE id=${id}`;
+    const res = await db.query(query, values);
+    if (res) {
+      res.status(500).json(res);
+    }
+    res.status(200).json({ update: true });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+app.delete("/delete/:table/:id", async (req, res) => {
+  try {
+    const table_name = req.params.table;
+    const id = req.params.id;
+    const query = `DELETE FROM ${table_name} WHERE id = ${id}`;
+    const result = await db.query(query);
+    if (result.rowCount > 0) {
+      res.status(200).json({ delete: true });
+    } else {
+      res.status(500).json({ err: "No data to delete" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-app.post("/update/:table/:id", (req, res) => {});
-app.delete("/delete/:table/:id", (req, res) => {});
-
-app.listen(port, () => {
+app.listen(port, async () => {
+  db = await d.connect();
   console.log(`App listening at http://localhost:${port}`);
   configurator.config();
 });
--- schema_config.js
+++ schema_config.js
@@ -28,33 +28,31 @@ async function config() {
     return await db.query(addCols);
   }
   // prettier-ignore
-  Object.keys(schemaFile).forEach(async(table) => {
-    try {  
+  Object.keys(schemaFile).forEach(async (table) => {
+    try {
       const exist = await checkExistence(table);
-      console.log(exist," ",table);
       if (!exist) {
         const res = await createTable(table);
-        console.log(res)
       } else {
         const resultSet = await checkColumns(table);
         const columnNames = resultSet.rows.map((row) => row.column_name.trim());
-        let columns = schemaFile[table].map((row) => { return row.split(" ")[0].trim().toLowerCase()});
+        let columns = schemaFile[table].map((row) => {
+          return row.split(" ")[0].trim().toLowerCase();
+        });
         let newCols = [];
-            for (let i in columns) {
-              if (!columnNames.includes(columns[i])) {
-                newCols.push(columns[i]);
-              }
-            }
-        console.log(newCols)
+        for (let i in columns) {
+          if (!columnNames.includes(columns[i])) {
+            newCols.push(columns[i]);
+          }
+        }
         let rese = [];
         for (let i of schemaFile[table]) {
           if (newCols.includes(i.split(" ")[0].toLowerCase())) {
-              rese.push(i);
+            rese.push(i);
           }
         }
         if (rese.length > 0) {
-          const res = await alterTable(table,rese);
-          console.log(res)
+          const res = await alterTable(table, rese);
         }
       }
     } catch (err) {
@@ -65,7 +63,4 @@ async function config() {
 
 module.exports = {
   config,
-  demo: () => {
-    console.log("Test");
-  },
 };
